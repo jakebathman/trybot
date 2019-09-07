05abdf0b85039ca32f082703862acb006e372fff
jakebathman <jake.bathman@gmail.com>
2019-09-07 14:00:59 -0500

    Fix JsonResponse using wrong Response

--- app/Http/Controllers/JsonResponse.php
+++ app/Http/Controllers/JsonResponse.php
@@ -1,23 +1,25 @@
 <?php
 namespace App\Http\Controllers;
 
-class JsonResponse extends \Response
+use Illuminate\Support\Facades\Response;
+
+class JsonResponse extends Response
 {
     public static function success($data)
     {
         $status = 'success';
-        return \Response::json(compact('status', 'data'));
+        return Response::json(compact('status', 'data'));
     }
 
     public static function fail($data)
     {
         $status = 'fail';
-        return \Response::json(compact('status', 'data'));
+        return Response::json(compact('status', 'data'));
     }
 
     public static function error($message, $code = null, $data = null)
     {
         $status = 'error';
-        return \Response::json(compact('status', 'message', 'code', 'data'));
+        return Response::json(compact('status', 'message', 'code', 'data'));
     }
 }
