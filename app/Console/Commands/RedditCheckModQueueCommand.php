4988aa40e93db322123bba21499381e365626064
jakebathman <jake.bathman@gmail.com>
2020-03-22 00:02:40 -0500

    Add slack channel mod queue clearing

--- app/Console/Commands/RedditCheckModQueueCommand.php
+++ app/Console/Commands/RedditCheckModQueueCommand.php
@@ -3,6 +3,8 @@
 namespace App\Console\Commands;
 
 use App\Http\Remotes\Reddit;
+use App\Http\Remotes\Slack;
+use App\Notifications\RedditModQueueClear;
 use App\Notifications\RedditModQueueItem;
 use Illuminate\Console\Command;
 use Illuminate\Support\Arr;
@@ -14,12 +16,14 @@ class RedditCheckModQueueCommand extends Command
     protected $signature = 'reddit:check-modqueue';
     protected $description = 'Check for new items in the modqueue and alert if any are found.';
     protected $reddit;
+    protected $slackChannel;
 
     public function __construct(Reddit $reddit)
     {
         parent::__construct();
 
         $this->reddit = $reddit;
+        $this->slackChannel = config('services.slack.meowbot.modqueue_channel');
     }
 
     public function handle()
@@ -30,20 +34,41 @@ class RedditCheckModQueueCommand extends Command
 
         $this->line('Posts: ' . count($posts));
 
-        $this->line('Sending Slack notifications...');
-        $posts->each(function ($post) {
-            $postId = Arr::get($post, 'data.id');
-            if ($this->hasBeenSent($postId)) {
-                $this->comment('Skipping: ' . Arr::get($post, 'data.title'));
-            } else {
-                $this->info('Sending: ' . Arr::get($post, 'data.title'));
-                
-                Notification::route('slack', config('services.slack.modqueue_webhook'))
-                ->notifyNow(new RedditModQueueItem($post));
+        if ($posts->isEmpty()) {
+            if (! $this->hasBeenCleared($this->slackChannel)) {
+                $this->line("Clearing existing Slack messages");
 
-                $this->logSent($postId);
+                $slack = new Slack($this->slackChannel);
+
+                collect($slack->getConversationHistory())
+                ->each(function ($message) use ($slack) {
+                    return $slack->deleteMessage($message['ts']);
+                });
+            
+                $this->info('Successfully deleted all messages');
+                Notification::route('slack', config('services.slack.modqueue_webhook'))
+                ->notifyNow(new RedditModQueueClear);
+
+                $this->logCleared($this->slackChannel);
             }
-        });
+        } else {
+            $this->unLogCleared($this->slackChannel);
+            
+            $this->line('Sending Slack notifications...');
+            $posts->each(function ($post) {
+                $postId = Arr::get($post, 'data.id');
+                if ($this->hasBeenSent($postId)) {
+                    $this->comment('Skipping: ' . Arr::get($post, 'data.title'));
+                } else {
+                    $this->info('Sending: ' . Arr::get($post, 'data.title'));
+                
+                    Notification::route('slack', config('services.slack.modqueue_webhook'))
+                        ->notifyNow(new RedditModQueueItem($post));
+
+                    $this->logSent($postId);
+                }
+            });
+        }
     }
 
     protected function hasBeenSent($id)
@@ -56,5 +81,22 @@ class RedditCheckModQueueCommand extends Command
     protected function logSent($id)
     {
         Redis::set('reddit:check-modqueue:' . $id, true);
+    }
+
+    protected function hasBeenCleared($id)
+    {
+        if (Redis::get('reddit:check-modqueue:channel-cleared:' . $id)) {
+            return true;
+        }
+    }
+
+    protected function logCleared($id)
+    {
+        Redis::set('reddit:check-modqueue:channel-cleared:' . $id, true);
+    }
+
+    protected function unLogCleared($id)
+    {
+        Redis::del('reddit:check-modqueue:channel-cleared:' . $id);
     }
 }
